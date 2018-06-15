118bd5f6cfced28e77ecca394933581ccc4eb8f2
Ashvini <ashvinikumar45@gmail.com>
2018-06-15 17:05:32 +0530

    displying category name with respective menu items

--- src/components/Detail/Detail.jsx
+++ src/components/Detail/Detail.jsx
@@ -5,12 +5,9 @@ import classes from './Detail.css';
 class Detail extends Component {
   render() {
     const menuData = [...this.props.menus];
-    // console.log(menuData);
-
     const categorizedData = menuData
       .map(menu => menu)
       .reduce((categories, menu) => {
-        // console.log("Hello", menu)
         categories[menu.category] = categories[menu.category] || []
         categories[menu.category].push({
           description: menu.description,
@@ -20,22 +17,28 @@ class Detail extends Component {
           vegflag: menu.vegflag,
         })
         return categories
-      }, [])
+      }, {})
 
-    console.log(categorizedData);
+    // console.log(categorizedData);
 
-    return this.props.menus.map((m, index) => (
-      <div key={index} className={classes.MenuItem}>
-        <div className={classes.MenuItemContainer}>
-          <div>
-            <img src={m.image} alt={m.name} className={classes.MenuImage} />
-          </div>
-          <span>
-            <h6 style={{ "float": "left" }}>{m.name}</h6>
-            <h6 style={{ "float": "right" }}>{m.price}</h6>
-          </span>
+    return (Object.keys(categorizedData).map((key, index) => (
+      <div key={index}>
+        <h2 className={classes.ItemCategory} >{key.toUpperCase()}</h2>
+        <div className={classes.Items}>
+          {categorizedData[key].map((key2, index) => (
+            <div key={index} className={classes.MenuItem}>
+              <div className={classes.MenuItemContainer}>
+                <img src={key2.image} alt={key2.name} className={classes.MenuImage} />
+              </div>
+              <span>
+                <h6 style={{ "float": "left" }}>{key2.name}</h6>
+                <h6 style={{ "float": "right" }}>{key2.price}</h6>
+              </span>
+            </div>
+          ))}
         </div>
       </div>
+    )
     ));
   }
 }
