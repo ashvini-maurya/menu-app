7f8d58ef82838cba795d4cd867da05e9dc82cdce
Ashvini <ashvinikumar45@gmail.com>
2018-06-25 02:56:27 +0530

    incomplete signup

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -3,16 +3,20 @@ import React, { Component } from 'react';
 import classes from './Login.css';
 import Modal from '../UI/Modal/Modal';
 
+import SignUp from '../SignUp/SignUp';
+
 class Login extends Component {
   constructor(props) {
     super(props);
     this.state = {
       email: '',
-      password: ''
+      password: '',
+      isLogin: true
     };
 
     this.handleChange = this.handleChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
+    this.signUpClicked = this.signUpClicked.bind(this);
   }
 
   handleChange(event) {
@@ -28,27 +32,40 @@ class Login extends Component {
     console.log("Entered Password: ", this.state.password);
   }
 
+  signUpClicked = (event) => {
+    console.log("signup event", event)
+    this.setState({
+      isLogin: false
+    })
+  }
+
   render() {
-    return (
-      <Modal show={this.props.show} clicked={this.props.modalClosed} title="Login">
-        <form className={classes.LoginForm} onSubmit={this.handleSubmit}>
-          <input
-            name="email"
-            type="email"
-            placeholder="Enter email"
-            onChange={this.handleChange}
-          />
-          <input
-            name="password"
-            type="password"
-            placeholder="Enter password"
-            onChange={this.handleChange}
-          />
-          <input type="submit" value="Login" />
-        </form>
-        <h4>Not Signed-up yet? Sign-up now <button onClick={this.signUpClicked}>Sign up</button></h4>
-      </Modal>
-    )
+    console.log("before render signupclicked status: ", this.state.isLogin)
+    if (!this.state.isLogin) {
+      return <SignUp />
+    }
+    else {
+      return (
+        <Modal show={this.props.show} clicked={this.props.modalClosed} title="Login">
+          <form className={classes.LoginForm} onSubmit={this.handleSubmit}>
+            <input
+              name="email"
+              type="email"
+              placeholder="Enter email"
+              onChange={this.handleChange}
+            />
+            <input
+              name="password"
+              type="password"
+              placeholder="Enter password"
+              onChange={this.handleChange}
+            />
+            <input type="submit" value="Login" />
+          </form>
+          <h4>Not Signed-up yet? Sign-up now <button onClick={this.signUpClicked}>Sign up</button></h4>
+        </Modal>
+      )
+    }
   }
 }
 
